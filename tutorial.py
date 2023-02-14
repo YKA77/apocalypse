440e82ef2754f14aaa680498ed4ea20b9f6df2d3
YKA77 <90383377+YKA77@users.noreply.github.com>
2023-02-14 19:07:39 +0000

    moving zombie sprite

--- tutorial.py
+++ tutorial.py
@@ -11,6 +11,10 @@ bg_test = pygame.image.load('images/cave.png')
 bg2_test = pygame.image.load('images/cavestructures.png')
 killcount = font_test.render('kills:', False, 'White')
 
+zombie_surface = pygame.image.load('images/zombie/zombie1.png')
+zombie_x_pos = 600
+zombie_y_pos = 40
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -19,7 +23,10 @@ while True:
 
     screen.blit(bg_test, (0, 0))
     screen.blit(bg2_test, (250, 0))
-    screen.blit(killcount, (10, 550))
+    screen.blit(killcount, (10, 550))   
+    zombie_x_pos -= 1
+    if zombie_x_pos < 480: zombie_x_pos = 600
+    screen.blit(zombie_surface, (zombie_x_pos, zombie_y_pos))
     
     pygame.display.update()
     clock.tick(60)
