22be08a36d0e01b697a3ae7eeb95fad6d56df78b
YKA77 <90383377+YKA77@users.noreply.github.com>
2023-03-20 08:04:11 +0000

    Update game.py

--- game.py
+++ game.py
@@ -1,42 +1,36 @@
-import pygame
-from sys import exit
-from config import *
+import pygame # import pygame commands and functions
+from config import * # import variables from config.py
+import sys
 
-pygame.init()
-screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption('Apocalypse')
-clock = pygame.time.Clock()
-done = False
-clock.tick(60)
-font_test = pygame.font.Font(None, 30)
-keys = pygame.key.get_pressed()
+class Game: # class for the actual game
+    def __init__(self):
+        self.screen = pygame.display.set_mode(window_width, window_height) # screen resolution 
+        self.clock = pygame.time.Clock() 
+        self.running = True # game is currently running
 
-class zombie:
-    def __init__(self, surface, rect, x_pos, y_pos):
-        self._surface = surface
-        self._rect = rect
-        self._x_pos = x_pos
-        self._y_pos = y_pos
+    def createTileMap(self): # create the map of the game
+        pass
 
-'''class player:
-    def __init__(self, surface, rect x_pos, y_pos)
-        self'''
+    def create(self):
+        pass
 
-bg_menu = pygame.image.load('images/menutest.png')
-bg_test = pygame.image.load('images/cave.png')
-bg2_test = pygame.image.load('images/cavestructures.png')
-killcount = font_test.render('kills:', False, 'White')
+    def update(self): # update movement and changes in every frame
+        pass
 
-def main_menu():
-    screen.blit(bg_menu, (0, 0))
-    if keys[pygame.K_SPACE]:
-        print("hello world")
-    
-def game():
-    screen.blit(bg_test, (0, 0))
-    screen.blit(bg2_test, (250, 0))
-    screen.blit(killcount, (10, 550))
-    screen.blit(zombie._surface, (600, 40))
-       
+    def events(self): # handles all inputs 
+        pass
 
-        
+    def draw(self): # draws the sprites on each frame
+        pass
+
+    def main(self):
+        pass
+
+game = Game()
+game.create() # start running an instance of the game
+
+while game.running == True: # only run the game when the running attribute is set to True
+    game.main()
+
+pygame.quit()
+sys.exit() 
