10ad30a9f115818075269f8d56843ee6cf92669a
YKA77 <90383377+YKA77@users.noreply.github.com>
2023-03-20 12:55:52 +0000

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -2,9 +2,25 @@ import pygame
 from main import *
 from config import *
 
-class Wall(pygame.sprite.Sprite):
-    def __init__(self, game, x, y)
+class Spritesheet(): # class for spritesheets
+    def __init__(self, path): # initialise the path of the spritesheet
+
+        self.spritesheet = pygame.image.load(file).convert() # load the spritesheet to be used
+
+    def get_image(self, x, y, width, height):
+        sprite = pygame.Surface([width, height]) # create the surface for the image
+        sprite.blit(spritesheet, (0, 0), (x, y, width, height) ) # loads image onto surface
+
+        return sprite
+    
+
+
+class Wall(pygame.sprite.Sprite): # class for the wall block
+    def __init__(self, game, x, y):
         
         self.game = game
-        self.x = x*tilesize
-        self.y = y*tilesize
+        self.x = x*tilesize # each x position will be every 32 pixels
+        self.y = y*tilesize # each y position will be every 32 pixels
+
+        self.width = tilesize # width is 32 pixels
+        self.height = tilesize # height is 32 pixels
