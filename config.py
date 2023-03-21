82d6fb88fb3e89c5e4807d32466e199fbbd607ad
YKA77 <90383377+YKA77@users.noreply.github.com>
2023-03-21 10:24:17 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ window_height = 600 # height of the game window
 
 fps = 60 # frames per second
 
+black = (0,0,0)
+
+exit_layer = 3 
+wall_layer = 2
+ground_layer = 1
+
 tilesize = 32 # each tile in the tilemap will be 32 pixels
 tilemap = [  
     'WWWWWWWWWEEWWWWWWWWW',
